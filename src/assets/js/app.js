6d2269766997e38aefda6ef4c8ab85bcdeb9df24
Pavel Sherer <pavel@sherer.pro>
2019-01-21 13:41:41 +0300

    html

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import 'what-input';
+import 'slick-carousel';
 
 // Foundation JS relies on a global varaible. In ES6, all imports are hoisted
 // to the top of the file so if we used`import` to import Foundation,
@@ -12,6 +13,29 @@ require('foundation-sites');
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
+$('#slick').slick({
+    speed: 500,
+    centerMode: true,
+    infinite: false,
+    slidesToShow: 4,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            centerMode: false,
+            slidesToShow: 3,
+        }
+    }, {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 2,
+            dots: true
+        }
+    }, {
+        breakpoint: 350,
+        slidesToShow: 2,
+        dots: true
 
+    }]
+});
 
 $(document).foundation();
